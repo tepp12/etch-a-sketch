3f6f978e74c5c252d6d48f4a68542d888a36059e
Guilherme Rêgo Ribeiro <yoshiribeiro@gmail.com>
2025-08-26 12:14:34 -0300

    add stupid squares
    stupid yet stupider

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
 
-divAmount = 256
-gridSize = Math.sqrt(divAmount)
+//later change divAmount to a custom var
+divAmount = 16*16;
+gridSize = Math.sqrt(divAmount);
 
 for (let i = 0; divAmount > i; i++){
     let myDiv = document.createElement("div")
     myDiv.classList.add("square")
     container.appendChild(myDiv)
+};
+
+const square = document.querySelectorAll(".square");
+
+square.forEach(el => el.addEventListener("mouseenter", event =>
+{
+    event.target.style.backgroundColor = "white"
 }
+))
