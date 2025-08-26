c5cd29cf9b8d5f1b00ea8a5cb7fca70b17ce1735
Guilherme Rêgo Ribeiro <yoshiribeiro@gmail.com>
2025-08-26 16:20:20 -0300

    Add initial sketch system

--- script.js
+++ script.js
@@ -1,19 +1,59 @@
 const container = document.querySelector("#container");
 
-//later change divAmount to a custom var
-divAmount = 16*16;
-gridSize = Math.sqrt(divAmount);
+const btn = document.querySelector("button");
 
-for (let i = 0; divAmount > i; i++){
+let divSize
+
+function InsertTiles(x){
+    for (let i = 0; x > i; i++){
     let myDiv = document.createElement("div")
     myDiv.classList.add("square")
     container.appendChild(myDiv)
 };
+};
 
-const square = document.querySelectorAll(".square");
-
-square.forEach(el => el.addEventListener("mouseenter", event =>
-{
-    event.target.style.backgroundColor = "white"
+function DeleteTiles(desiredClass){
+    desiredClass.each(function(element){
+        element.remove()
+    })
 }
-))
+
+function AddListeners(desiredClass){
+    desiredClass.forEach(el => el.addEventListener("mouseover", event =>
+    {
+    if(event.buttons == 1 || event.buttons == 3){
+        event.target.style.backgroundColor = "white"
+    }
+    }
+));
+    desiredClass.forEach(el => el.addEventListener("mousedown", event => {
+        event.target.style.backgroundColor = "white"
+        }
+    ));
+}
+
+btn.addEventListener("click", event => {
+    let gridTile = +window.prompt("GIMME THE NUMBER OF SQUARES PER GRID")
+    let divAmount = gridTile*gridTile;
+    
+    // add check to see if there's any children inside container, then delete
+    //DeleteTiles(square)
+
+    if (typeof gridTile != "number"){
+        alert("NOT NUMBER PLEASE INPUT ANOTHER ONE")
+        return
+    }
+    if (gridTile > 100){
+        alert("DO YOU WANT TO LAG YOUR PC? PLEASE INPUT ANOTHER ONE")
+        return
+    }
+
+    if (Math.sign(gridTile) === 1){
+        InsertTiles(divAmount)
+        let square = document.querySelectorAll(".square")
+        AddListeners(square)
+
+    } else{
+        alert("ENTER POSITIVE NUMBER, DUDE")
+    }
+})
